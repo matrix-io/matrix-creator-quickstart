3ab0dd66c24c81a1a9da63f000fc2061a9a41c2c
Brian Sanchez <brian@rokk3rlabs.com>
2016-07-20 10:46:20 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "deb http://packages.matrix.one/matrix-creator/ ./" | sudo tee --append /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get install -y xc3sprog matrix-creator-openocd wiringpi matrix-creator-init cmake g++ git
+sudo apt-get install -y xc3sprog matrix-creator-openocd wiringpi matrix-creator-init cmake g++ git --force-yes
