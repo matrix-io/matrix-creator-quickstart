a7732fe2e4c78fbb22b7447e6493f88b3abec293
Brian Sanchez <brian@rokk3rlabs.com>
2016-11-25 18:30:50 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
-echo "deb http://packages.matrix.one/matrix-creator/ ./" | sudo tee --append /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get install -y xc3sprog matrix-creator-openocd wiringpi matrix-creator-init cmake g++ git --force-yes
+# setup & installation
+echo "deb http://packages.matrix.one/matrix-creator/ ./" | sudo tee --append /etc/apt/sources.list;
+sudo apt-get update;
+sudo apt-get upgrade;
+sudo apt-get install -y libzmq3-dev xc3sprog malos-eye matrix-creator-malos matrix-creator-openocd wiringpi matrix-creator-init cmake g++ git --force-yes;
+
+# Install npm (doesn't really matter what version, apt-get node is v0.10...)
+sudo apt-get install npm
+
+# n is a node version manager
+sudo npm install -g n
+
+# node 6.5 is the latest target node version, also installs new npm
+n 6.5
+
+# install matrix os
+git clone https://github.com/matrix-io/matrix-os.git;
+cd matrix-os;
+git submodule update --init;
+npm install;
+
+# Reboot!
+sudo reboot;
