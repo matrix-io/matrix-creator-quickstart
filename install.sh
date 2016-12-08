201d8c8c6a159472156de5613b62ec9a7d7cc8ab
Sean Canton <eighteyes@gmail.com>
2016-12-08 10:17:07 -0800

    Break out git

--- install.sh
+++ install.sh
@@ -8,7 +8,9 @@ echo '\n'
 echo "deb http://packages.matrix.one/matrix-creator/ ./" | sudo tee --append /etc/apt/sources.list;
 sudo apt-get update;
 sudo apt-get upgrade;
-sudo apt-get -y install libzmq3-dev xc3sprog malos-eye matrix-creator-malos matrix-creator-openocd wiringpi matrix-creator-init cmake g++ git;
+sudo apt-get -y install git;
+
+sudo apt-get -y install libzmq3-dev xc3sprog malos-eye matrix-creator-malos matrix-creator-openocd wiringpi matrix-creator-init cmake g++;
 
 # Install npm (doesn't really matter what version, apt-get node is v0.10...)
 sudo apt-get -y install npm
