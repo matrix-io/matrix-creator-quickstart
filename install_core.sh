de328b2299e186bc63aa3dded4fcb62128889a0e
Andres Calderon <andres.calderon@admobilize.com>
2018-02-01 16:24:53 -0500

    install core script for MATRIX Voice/Creator

---
+++ install_core.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# setup & installation
+echo '\n\n'
+echo '---------------------------------'
+echo 'Installing MATRIX Dependencies...'
+echo '---------------------------------'
+
+curl https://apt.matrix.one/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.matrix.one/raspbian $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/matrixlabs.list
+
+# Update packages and install
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get -y install git cmake g++;
+sudo apt-get -y install libzmq3-dev;
+
+# creator code needs force until it can be authenticated
+sudo apt-get -y --force-yes install matrixio-malos \
+	matrixio-malos-wakeword \
+	matrixio-malos-zigbee
+
+echo '\n\n'
+echo '---------------------------------'
+echo 'Rebooting... Please re-connect to'
+echo ' your Raspberry Pi in a minute.  '
+echo '---------------------------------'
+echo '\n'
+sudo reboot;
